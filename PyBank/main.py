035aff3fb2f7b6091bceb61b6869607fa2179156
raciv65 <70599195+raciv65@users.noreply.github.com>
2020-10-02 20:06:28 -0500

    Update main.py
    Adding format

--- PyBank/main.py
+++ PyBank/main.py
@@ -45,7 +45,7 @@ Total Months {MonthsTotal}
 Total: {GainTotal:,} 
 Averange Change: $ {round(averange(Delta),2):,}
 Greatest Increase in Profits: {Results[0][0]} ( $ {Results[0][1]:,})
-Greatest Decrease in Profits: {Results[len(Results)-1][0]} ( $ {Results[len(Results)-1][1]})
+Greatest Decrease in Profits: {Results[len(Results)-1][0]} ( $ {Results[len(Results)-1][1]:,})
 """)
 
 SavePath=os.path.join("Analysis","Financial Analysis.txt")
