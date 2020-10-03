d90437e0bca2b6c3422c647ed4522034896563a5
raciv65 <70599195+raciv65@users.noreply.github.com>
2020-10-02 21:55:20 -0500

    Update write in txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,11 +22,27 @@ ElectionsResults=sorted(Summary, key=lambda x: x[1], reverse=True)
 
 print(f'''Election Results
 -----------------------------------------------
-Total Votes: {int(total)}
+Total Votes: {int(total):,}
 -----------------------------------------------''')
 for CandidateResults in ElectionsResults:
-    print(f'{CandidateResults[0]}  : {round(((CandidateResults[1]/total)*100),2)}% ( {CandidateResults[1]})')
+    percentCandidate=(CandidateResults[1]/total)
+    print(f'{CandidateResults[0]}  : {percentCandidate:.2%} ( {CandidateResults[1]:,})')
 print(f'''-----------------------------------------------
 Winner: {ElectionsResults[0][0]}
 -----------------------------------------------''')
 
+ElectionsFile=os.path.join("Analysis","Election_Results.txt")
+
+with open(ElectionsFile, "w", newline='', encoding="UTF-8") as datafile:
+    writer=csv.writer(datafile)
+    writer.writerow(['Election Results'])
+    writer.writerow(['-----------------------------------------------'])
+    writer.writerow(['Total Votes: {:n}'.format(total)])
+    writer.writerow(['-----------------------------------------------'])
+
+    for row in ElectionsResults:
+        writer.writerow([str(row[0])+': {:.2%}'.format(row[1]/total)]+' ( {:n})'.format(row[1]))
+
+    writer.writerow(['-----------------------------------------------'])
+    writer.writerow(['Winner: '+str(ElectionsResults[0][0])])
+    writer.writerow(['-----------------------------------------------'])
